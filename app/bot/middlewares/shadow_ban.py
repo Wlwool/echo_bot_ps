69e24b0be0e5301e898ac289a7210222e09f3369
Wlwool <blackenedima@yandex.ru>
2025-07-03 13:40:47 +0300

    проверка на бан пользователя

--- app/bot/middlewares/shadow_ban.py
+++ app/bot/middlewares/shadow_ban.py
@@ -10,11 +10,12 @@ logger = logging.getLogger(__name__)
 
 
 class ShadowBanMiddleware(BaseMiddleware):
-    async def __call__(self,
+    async def __call__(
+            self,
             handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
             event: Update,
-            data: dict[str, Any],) -> Any:
-
+            data: dict[str, Any],
+    ) -> Any:
         user: User = data.get("event_from_user")
 
         if user is None:
@@ -25,12 +26,14 @@ class ShadowBanMiddleware(BaseMiddleware):
             logger.error("Database connection not found in middleware data.")
             raise RuntimeError("Missing database connection for shadow ban check.")
 
-        user_banned_status = await get_user_banned_status_by_id(conn, user_id=user.id)
-
-        if user_banned_status:
-            logger.warning("Shadow-banned user tried to interact: %d", user.id)
-            if event.callback_query:
-                await event.callback_query.answer()
-            return
+        try:
+            user_banned_status = await get_user_banned_status_by_id(conn, user_id=user.id)
+            if user_banned_status:
+                logger.warning("Shadow-banned user tried to interact: %d", user.id)
+                if event.callback_query:
+                    await event.callback_query.answer()
+                return
+        except Exception as e:
+            logger.error(f"Error in shadow ban check: {e}")
 
         return await handler(event, data)
