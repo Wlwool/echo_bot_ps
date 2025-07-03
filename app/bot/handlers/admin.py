3b7d37a94dfa553a4b7d781afe5d57c3a252bf36
Wlwool <blackenedima@yandex.ru>
2025-07-03 13:32:46 +0300

    formatting

--- app/bot/handlers/admin.py
+++ app/bot/handlers/admin.py
@@ -20,52 +20,64 @@ admin_router = Router()
 
 admin_router.message.filter(UserRoleFilter(UserRole.ADMIN))
 
-# Этот хэндлер будет срабатывать на команду /help для пользователя с ролью 'UserRole.ADMIN'
-@admin_router.message(Command("help"))
-async def process_admin_help_command(message: Message, i18n: dict):
-    await message.answer(text=i18n.get("/help_admin"))
 
-# Этот хэндлер будет срабатывать на команду /statistics для пользователя с ролью 'UserRole.ADMIN'
+# Этот хэндлер будет срабатывать на команду /help для пользователя с ролью `UserRole.ADMIN`
+@admin_router.message(Command('help'))
+async def process_admin_help_command(message: Message, i18n: dict):
+    await message.answer(text=i18n.get('/help_admin'))
+
+
+# Этот хэндлер будет срабатывать на команду /statistics для пользователя с ролью `UserRole.ADMIN`
 @admin_router.message(Command('statistics'))
 async def process_admin_statistics_command(message: Message, conn: AsyncConnection, i18n: dict[str, str]):
     statistics = await get_statistics(conn)
-    await message.answer(text=i18n.get("statistics").format("\n".join(f"{i}. <b>{stat[0]}</b>: {stat[1]}"
-                for i, stat in enumerate(statistics, 1)))
+    await message.answer(
+        text=i18n.get("statistics").format(
+            "\n".join(
+                f"{i}. <b>{stat[0]}</b>: {stat[1]}"
+                for i, stat in enumerate(statistics, 1)
+            )
+        )
     )
 
-# Этот хэндлер будет срабатывать на команду /ban для пользователя с ролью 'UserRole.ADMIN'
+
+# Этот хэндлер будет срабатывать на команду /ban для пользователя с ролью `UserRole.ADMIN`
 @admin_router.message(Command("ban"))
 async def process_ban_command(
-        message: Message, command: CommandObject, conn: AsyncConnection, i18n: dict[str, str]) -> None:
+        message: Message,
+        command: CommandObject,
+        conn: AsyncConnection,
+        i18n: dict[str, str]
+) -> None:
     args = command.args
 
     if not args:
-        await message.reply(i18n.get("empty_ban_answer"))
+        await message.reply(i18n.get('empty_ban_answer'))
         return
 
     arg_user = args.split()[0].strip()
 
     if arg_user.isdigit():
         banned_status = await get_user_banned_status_by_id(conn, user_id=int(arg_user))
-    elif arg_user.startswith("@"):
+    elif arg_user.startswith('@'):
         banned_status = await get_user_banned_status_by_username(conn, username=arg_user[1:])
     else:
-        await message.reply(text=i18n.get("incorrect_ban_arg"))
+        await message.reply(text=i18n.get('incorrect_ban_arg'))
         return
 
     if banned_status is None:
-        await message.reply(text=i18n.get("no_user"))
+        await message.reply(i18n.get('no_user'))
     elif banned_status:
-        await message.reply(text=i18n.get("already_banned"))
+        await message.reply(i18n.get('already_banned'))
     else:
         if arg_user.isdigit():
             await change_user_banned_status_by_id(conn, user_id=int(arg_user), banned=True)
         else:
             await change_user_banned_status_by_username(conn, username=arg_user[1:], banned=True)
-        await message.reply(text=i18n.get("successfully_banned"))
+        await message.reply(text=i18n.get('succesfully_banned'))
 
 
-# Этот хэндлер будет срабатывать на команду /unban для пользователя с ролью 'UserRole.ADMIN'
+# Этот хэндлер будет срабатывать на команду /unban для пользователя с ролью `UserRole.ADMIN`
 @admin_router.message(Command('unban'))
 async def process_unban_command(
         message: Message,
